23702acbd31d9232bb32a693660d1e8fb5f623e7
Dimitar11 <dvasilev@outlook.com>
2025-03-07 20:54:06 +0200

    Setup database, added Employee model, and applied migrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models; 
+
+namespace WebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
